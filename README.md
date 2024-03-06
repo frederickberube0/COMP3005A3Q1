86ffcb99fee79735140d43de0de6c126f208dbbf
frederickberube <frederickberube0@gmail.com>
2024-03-06 14:48:41 -0500

    error handling

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 Instructions to run:
 
 1. On postgres, create an empty database.
-2. Run the python file, `python3 main.py`
+2. Run the python file, `python3 src/main.py`
 3. Answer the prompts
 4. Run the commands
 
 Video: youtube
+
+Explanation of functions
+
+The main() function has the main loop and asks the user for their decisions, and if required, asks for the additional variables.
+
+1: getAllStudents() This function gets all the tuples from the student table and prints them to the console.
+2: addStudent(first_name, last_name, email, enrollment_date) This function adds the specified value to the students table.
+3: updateStudentEmail(student_id, new_email) This function updates the tuple with the specified student_id with a new email.
+4: deleteStudent(student_id) This function deletes the tuple with the specified student_id from the students table.
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import psycopg2
 from psycopg2 import sql
 
-
 # Database connection parameters
 dbname = input("database name (default=assignment_three): ")
 user = input("user name (default=postgres): ")
@@ -17,14 +16,17 @@ if port == "": port = "5432"
 
 print("")
 # Create a connection to the database
-conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-
+try: 
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+except psycopg2.Error as e:
+                print(f"Error running sql command: {e}")
+                exit()
+                
 # Create a cursor object to execute SQL queries
 cursor = conn.cursor()
 
 def main():
-
-    with open("students.sql", 'r') as file:
+    with open("./sql/students.sql", 'r') as file:
         # Read the contents of the file into a string
         initial_query = file.read()
 
@@ -55,28 +57,46 @@ What is your decision?: """)
             conn.close()
             
         elif decision == "1":
-            getAllStudents()
+            try: 
+                getAllStudents()
+            except psycopg2.Error as e:
+                print(f"Error running sql command: {e}")
+                conn.rollback()
+
 
         elif decision == "2": 
             first_name = input("What is the first name: ")
             last_name = input("What is the last name: ")
             email = input("What is the email: ")
             enrollment_date = input("What is the enrollment date in format 'yyyy-mm-dd': ")
-            addStudent(first_name, last_name, email, enrollment_date)
+            try:
+                addStudent(first_name, last_name, email, enrollment_date)
+            except psycopg2.Error as e:
+                print(f"Error running sql command: {e}")
+                conn.rollback()
 
         elif decision == "3":
             student_id = input("What is the student id: ")
             new_email = input("What is the new email: ")
-            updateStudentEmail(student_id, new_email)
+            try:
+                updateStudentEmail(student_id, new_email)
+            except psycopg2.Error as e:
+                print(f"Error running sql command: {e}")
+                conn.rollback()
 
         else:
             student_id = input("What is the student id: ")
-            deleteStudent(student_id)
-    
-    # Close the cursor and connection
+            try:
+                deleteStudent(student_id)
+            except psycopg2.Error as e:
+                print(f"Error running sql command: {e}")
+                conn.rollback()
+        print()
 
-def getAllStudents():
-    # Execute the query to create the table
+
+def getAllStudents() -> None:
+    """This function gets all the tuples from the student table and prints them to the console."""
+    # Execute the query to select all from the table
     cursor.execute("SELECT * FROM students;")
 
     # Fetch all rows from the result set
@@ -88,15 +108,18 @@ def getAllStudents():
 
     conn.commit()
 
-def addStudent(first_name, last_name, email, enrollment_date):
-    cursor.execute(f"INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ('{first_name}', '{last_name}', '{email}', '({enrollment_date}');")
+def addStudent(first_name, last_name, email, enrollment_date) -> None:
+    """This function adds the specified value to the students table."""
+    cursor.execute(f"INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ('{first_name}', '{last_name}', '{email}', '{enrollment_date}');")
     conn.commit()
 
-def updateStudentEmail(student_id, new_email):
+def updateStudentEmail(student_id, new_email) -> None:
+    """This function updates the tuple with the specified student_id with a new email."""
     cursor.execute(f"UPDATE students SET email = '{new_email}' WHERE student_id = {student_id};")
     conn.commit()
 
-def deleteStudent(student_id):
+def deleteStudent(student_id) -> None:
+    """This function deletes the tuple with the specified student_id from the students table."""
     cursor.execute(f"DELETE FROM students WHERE student_id = '{student_id}';")
     conn.commit()
 
